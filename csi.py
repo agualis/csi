47d386543856462eb442a9cb39a00224ba10b19c
Alberto Gualis <agualiss@gmail.com>
2016-05-23 21:28:33 +0200

    Generating revisions

--- csi.py
+++ csi.py
@@ -8,14 +8,14 @@ merge_maat = 'python ~/code-maat/scripts\ 4/merge_comp_freqs.py'
 
 
 @click.command()
-@click.option('--name', prompt='Your codebase name',
+@click.option('--project_name', prompt='Your codebase name',
               help='The name of your project')
 @click.option('--cvs', prompt='Your cvs type', help='git, hg or svn')
 @click.option('--after', default='1960-01-01', prompt='Starting date', help='Format: yyyy-mm-dd')
 @click.option('--before', default='2050-01-01', prompt='Finish date', help='Format: yyyy-mm-dd')
 
-def csi(name, cvs, after, before):
-    click.echo("Let's study your %s proyect saved in %s!" % (name, cvs))
+def csi(project_name, cvs, after, before):
+    click.echo("Let's study your %s proyect saved in %s!" % (project_name, cvs))
     click.echo("Starting Delorean...fasten your seatbelts")
     click.echo("We will travel from %s" % after)
     click.echo("We will stop at %s" % before)
@@ -23,7 +23,7 @@ def csi(name, cvs, after, before):
     click.echo("")
 
     if cvs == 'git':
-        return git_csi(name, after, before)
+        git_csi(project_name, after, before)
     else:
         click.echo("%s csv not supported [use git or hg]" % cvs)
         sys.exit(1)
@@ -33,17 +33,24 @@ def csi(name, cvs, after, before):
 def hg_csi(name, cvs, after, before):
     evolution = """hg log --template "rev: {rev} author: {author} date: {date|shortdate} files:\n{files %'{file}\n'}\n" > roi_evo.log"""
 
-def git_csi(name, after, before):
+def git_csi(project_name, after, before):
     evolution = "git log --pretty=format:'[%h] %aN %ad %s' --date=short --numstat --after={0} --before={1} > {2}_evo.log" \
-        .format(after, before, name)
+        .format(after, before, project_name)
 
-    summary = maat + " -l {0}_evo.log -c git -a summary > {0}_summary.txt".format(name)
+    summary = maat + " -l {0}_evo.log -c git -a summary > {0}_summary.txt".format(project_name)
+
+    revisions = maat + " -l {0}_evo.log -c git -a revisions > {0}_freq.csv".format(project_name)
 
     click.echo("Generating evolution...")
     call(evolution, shell=True)
 
     click.echo("Generating summary...")
     call(summary, shell=True)
+
+    click.echo("Generating revisions...")
+    call(revisions, shell=True)
+
+
 
 
 if __name__ == '__main__':
